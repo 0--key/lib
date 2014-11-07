b31ad7eb2b12e76a079bc6cf8fc1c1928d5a8155
Antony Kosinov <antony@cosmic.(none)>
2014-11-07 21:53:13 +0200

    Next step further

--- Python/GAE/server_test/main.py
+++ Python/GAE/server_test/main.py
@@ -1,5 +1,15 @@
+import json
+from decimal import Decimal
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
+
+
+def quremo(a, b):
+    """Primitive ariphmetic calculations"""
+    qu = Decimal(b) / Decimal(a)
+    re = int(qu)
+    mo = Decimal(b) % Decimal(a)
+    return str(qu), str(re), str(mo)
 
 
 class MainHandler(webapp.RequestHandler):
@@ -10,9 +20,11 @@ class MainHandler(webapp.RequestHandler):
 
 class DivideHandler(webapp.RequestHandler):
     def get(self, divisor, raw_dividend):
-        dividend = self.request.query_string
-        self.response.write('Hello world!\
-        This is the quotient page %s, %s' % (divisor, dividend))
+        dividend = self.request.query_string.split('=')[1]
+        (q, r, m) = quremo(divisor, dividend)
+        self.response.headers['Content-Type'] = 'application/json'
+        obj = {'quotient': q, 'result': r, 'modulus': m}
+        self.response.out.write(json.dumps(obj))
 
 
 application = webapp.WSGIApplication([
--- Python/GAE/server_test/unit_test.py
+++ Python/GAE/server_test/unit_test.py
@@ -1,42 +1,25 @@
-import random
+import urllib2
+import json
 import unittest
-
-
-class TestSequenceFunctions(unittest.TestCase):
-
-    def setUp(self):
-        self.seq = range(10)
-
-    def test_shuffle(self):
-        # make sure the shuffled sequence does not lose any elements
-        random.shuffle(self.seq)
-        self.seq.sort()
-        self.assertEqual(self.seq, range(10))
-
-        # should raise an exception for an immutable sequence
-        self.assertRaises(TypeError, random.shuffle, (1,2,3))
-
-    def test_choice(self):
-        element = random.choice(self.seq)
-        self.assertTrue(element in self.seq)
-
-    def test_sample(self):
-        with self.assertRaises(ValueError):
-            random.sample(self.seq, 20)
-        for element in random.sample(self.seq, 5):
-            self.assertTrue(element in self.seq)
 
 
 class LocalServerCase(unittest.TestCase):
 
     def test_gae_server(self):
+        #r = requests.get('http://localhost:8080')
         self.assertEqual(1, 1)
 
 
 class LocalServerAPICase(unittest.TestCase):
 
     def test_gae_API(self):
-        self.assertEqual(1, 1)
+        test_data = [(4, 10), (5, 12)]
+        T = True
+        for i, j in test_data:
+            url = 'http://localhost:8080/divide/%s/?dividend=%s' % (i, j)
+            r = urllib2.urlopen(url)
+            json_data = json.load(r)
+        self.assertTrue(T)
 
             
 if __name__ == '__main__':
