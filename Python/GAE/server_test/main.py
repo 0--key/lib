0ab63cdc1e6631a1e07f44d454172c53e385a40e
a.s.kosinov@gmail.com <a.s.kosinov@gmail.com>
2014-11-10 11:44:37 +0200

    Unit test and MemCache

--- Python/GAE/server_test/main.py
+++ Python/GAE/server_test/main.py
@@ -1,7 +1,11 @@
 import json
+import logging
 from decimal import Decimal
 from google.appengine.ext import webapp
+from google.appengine.api import memcache
+from google.appengine.ext import ndb
 from google.appengine.ext.webapp.util import run_wsgi_app
+from models import Results
 
 
 def quremo(a, b):
@@ -9,7 +13,23 @@ def quremo(a, b):
     qu = Decimal(b) / Decimal(a)
     re = int(qu)
     mo = Decimal(b) % Decimal(a)
+    logging.info("It's a log")
     return str(qu), str(re), str(mo)
+
+
+def get_quremo(a, b):
+    """Memcache or DataStore interaction implementation
+    with aim to avoid server calculations overload"""
+    a_key = a + '&' + b  # an unique key for each pair
+    cached_result = memcache.get(key=a_key)
+    if cached_result is None:
+        calc_val = quremo(a, b)
+        memcache.add(key=a_key, value=calc_val, time=3600)
+        logging.info("Data was cached successfully")
+    else:
+        calc_val = cached_result
+        logging.info("Data was retrieved out from MemCache")
+    return calc_val
 
 
 class MainHandler(webapp.RequestHandler):
@@ -21,7 +41,7 @@ class MainHandler(webapp.RequestHandler):
 class DivideHandler(webapp.RequestHandler):
     def get(self, divisor, raw_dividend):
         dividend = self.request.query_string.split('=')[1]
-        (q, r, m) = quremo(divisor, dividend)
+        (q, r, m) = get_quremo(divisor, dividend)
         self.response.headers['Content-Type'] = 'application/json'
         obj = {'quotient': q, 'result': r, 'modulus': m}
         self.response.out.write(json.dumps(obj))
--- Python/GAE/server_test/unit_test.py
+++ Python/GAE/server_test/unit_test.py
@@ -1,26 +1,46 @@
 import urllib2
 import json
 import unittest
+from decimal import Decimal
+
+
+def quremo(a, b):
+    """Primitive ariphmetic calculations"""
+    qu = Decimal(b) / Decimal(a)
+    re = int(qu)
+    mo = Decimal(b) % Decimal(a)
+    return str(qu), str(re), str(mo)
 
 
 class LocalServerCase(unittest.TestCase):
 
     def test_gae_server(self):
-        #r = requests.get('http://localhost:8080')
-        self.assertEqual(1, 1)
+        """GAE local server running test"""
+        W = True
+        url = 'http://localhost:8080'
+        try:
+            r = urllib2.urlopen(url)
+        except:
+            W = False
+        self.assertTrue(W, "Connection refused by server")
 
 
 class LocalServerAPICase(unittest.TestCase):
 
     def test_gae_API(self):
+        """Calculations correctness test"""
         test_data = [(4, 10), (5, 12)]
         T = True
         for i, j in test_data:
             url = 'http://localhost:8080/divide/%s/?dividend=%s' % (i, j)
             r = urllib2.urlopen(url)
             json_data = json.load(r)
-        self.assertTrue(T)
+            if quremo(i, j) != (json_data['quotient'],
+                                json_data['result'],
+                                json_data['modulus']):
+                T = False
+        self.assertTrue(T, "Calculations is incorrect")
 
-            
+
 if __name__ == '__main__':
     unittest.main()
