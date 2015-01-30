3345fb14bcd2c71d8baaa7a0ddd0e0a4a11a86d4
a.s.kosinov@gmail.com <a.s.kosinov@gmail.com>
2015-01-30 12:20:58 +0200

    Sqlite table reloader from the file

---
+++ Python/general/sqliteReload.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+"""Updates sqlite table with changed file's content
+"""
+
+conn = sqlite3.connect('server/mhn.db')
+ds_file = open('scripts/deploy_snort.sh', 'r')
+script = ds_file.read()
+u = (script,)
+conn.execute('UPDATE deploy_scripts SET script=? WHERE id=2', u)
+conn.commit()
