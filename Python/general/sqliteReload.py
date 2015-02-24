b32c7b3a8656b482982352a0d4dee182a934dd3d
a.s.kosinov@gmail.com <a.s.kosinov@gmail.com>
2015-02-24 11:39:29 +0200

    New catalogue data builder

--- Python/general/sqliteReload.py
+++ Python/general/sqliteReload.py
@@ -10,3 +10,30 @@ script = ds_file.read()
 u = (script,)
 conn.execute('UPDATE deploy_scripts SET script=? WHERE id=2', u)
 conn.commit()
+
+
+def get_catalogue():
+    """
+    Extracts catalogue metadata
+    catalogue_data = ({'name': 'cosmetics', 'count': 127, 'depth': 1}, ...)
+    """
+    conn = sqlite3.connect('scraped.db')
+    cur = conn.cursor()
+    # Prepare paginator data:
+    cur.execute('SELECT category FROM products_var_data')
+    raw_categories = cur.fetchall()
+    catalogue_data = ()
+    for i in raw_categories:
+        categories = i[0].split('/')[1:]
+        #print cat_list
+        d = 0  # deepth counter
+        for j in categories:  # fullfill the catalogue tree
+            for k in catalogue_data:
+                if k['name'] == j:  # this categore is already exists
+                    k['count'] = k['count'] + 1
+                    break
+            new_category = {'name': j, 'depth': d, 'count': 1}
+            catalogue_data = catalogue_data + (new_category,)
+            d = d + 1
+    conn.close()
+    return catalogue_data
