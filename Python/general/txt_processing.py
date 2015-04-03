8da3eccf1704bf24b1c62af771ba3c4f2f5bcccf
Anton Kosinov <a.s.kosinov@gmail.com>
2015-04-03 17:27:54 +0300

    Next improvements 01

--- Python/general/txt_processing.py
+++ Python/general/txt_processing.py
@@ -1,6 +1,8 @@
+# Replaces unrecognized symbols out from urls
+
 replacement = {"'": '%27', '.': '%2E', '+': '%2B', '`': '%60',
                '#': '%23', ' ': '%20', '%': '%25'}
-
+# append additional dictionary if it's necessary
 
 def purify_url(raw_url):
     """Replases prohibited symbols out from raw url"""
