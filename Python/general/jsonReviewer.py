e496c6001d049768cabadc03f99c09f80ad5dca9
a.s.kosinov@gmail.com <a.s.kosinov@gmail.com>
2015-04-07 20:35:33 +0300

    Temporary data allocation

--- Python/general/jsonReviewer.py
+++ Python/general/jsonReviewer.py
@@ -13,7 +13,7 @@ formatter = logging.Formatter(
     '%(asctime)s %(levelname)s %(message)s')
 hdlr.setFormatter(formatter)
 logger.addHandler(hdlr)
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 # Lets generate json out from sqlite3 DB with 29k unique items
 """
 sqlite> .schema products
@@ -47,29 +47,59 @@ class Product():
         return True
 
 
+# Initially create JSON file with data:
+
+def init_json_file_gen():
+    """
+    Generates JSON file out from DB
+    """
+    conn = sqlite3.connect('scraped.db')
+    cur = conn.cursor()
+    cur.execute('select * from products')
+    p_data = cur.fetchall()
+    for i in p_data:
+        msg = i
+        logging.info(msg)
+    conn.commit()
+    conn.close
+    print 'Job done'
+    return True
 # Define functionalyty in accordion with low RAM consumption
 # and allocate json into HD DB
 
 def conc_two_json_files(conn, json1, json2):
     """
     concatenates two json with unique rows only
-    by means md5 technology comparison
+    by means md5 technology comparison and temporary
+    sqlite3 data allocation
     """
     cur = conn.cursor()
     conn.commit()
     output_file_name = "date" + ".json"
-    logger.info('While this is just chatty')
+    msg = "This is filenames: %s, %s" % (json1, json2)
+    logger.info(msg)
+    logger.debug('While this is switch')
     return True
 
 
-def __init__(self):
-    print 'I have launched!'
+def conc_two_json_files_in_memory(json1, json2):
+    """
+    concatenates two json with unique rows only
+    by means md5 technology comparison and temporary
+    RAM data allocation
+    """
     return True
-
-print "I'm here!"
 
 # functional body is here
 
 if sys.argv[0]:
-    conn = sqlite3.connect('scraped.db')
-    conc_two_json_files(conn, 'file1', 'file2')
+    if sys.argv[1] == '-r':
+        # in-memory data temporary allocation
+        pass
+    if sys.argv[1] == '-d':
+        # hard way for big data analyse
+        # and unique data temporary storing into SQLite3 tables
+        # for further processing
+        conn = sqlite3.connect('tmp_data.db')
+        conc_two_json_files(conn, sys.argv[2], sys.argv[3])
+        conn.close()
