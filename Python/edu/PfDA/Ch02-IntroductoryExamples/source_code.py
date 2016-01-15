a9ea7eace4bb681bc9c4ec37908f2b481c4ecf80
a.s.kosinov@gmail.com <a.s.kosinov@gmail.com>
2016-01-15 12:11:06 +0200

    p23

--- Python/edu/PfDA/Ch02-IntroductoryExamples/source_code.py
+++ Python/edu/PfDA/Ch02-IntroductoryExamples/source_code.py
@@ -4,3 +4,11 @@ import pandas as pd
 path = 'pydata-book/ch02/usagov_bitly_data2012-03-16-1331923249.txt'
 records = [json.loads(line) for line in open(path)]
 frame = DataFrame(records)
+
+print frame['tz'][:10]  # unsorted
+print frame['tz'].value_counts()
+print '\n#   #\n'
+clean_tz = frame['tz'].fillna('Missing')
+clean_tz[clean_tz == ''] = 'Unknown'
+tz_counts = clean_tz.value_counts()
+print tz_counts[:10]
