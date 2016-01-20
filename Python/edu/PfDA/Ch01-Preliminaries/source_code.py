949b69321c2fe68f614b2bf77be69581baa46805
a.s.kosinov@gmail.com <a.s.kosinov@gmail.com>
2016-01-20 03:50:12 +0200

    Installation additional lib into py3 venv

--- Python/edu/PfDA/Ch01-Preliminaries/source_code.py
+++ Python/edu/PfDA/Ch01-Preliminaries/source_code.py
@@ -21,13 +21,13 @@ def top_counts(count_dict, n=10):
     value_key_pairs.sort()
     return value_key_pairs[-n:]
 
-print 'Hi there'
+print('Hi there')
 
 path = 'pydata-book/ch02/usagov_bitly_data2012-03-16-1331923249.txt'
 records = [json.loads(line) for line in open(path)]
-print records[0]
+print(records[0])
 time_zones = [rec['tz'] for rec in records if 'tz' in rec]
 # print set(time_zones), len(set(time_zones))
 
 counts = Counter(time_zones)
-print counts.most_common(10)
+print(counts.most_common(10))
