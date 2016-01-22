b04eac67e648c1ef2f570feeb953a6c1acdf841b
a.s.kosinov@gmail.com <a.s.kosinov@gmail.com>
2016-01-22 09:38:59 +0200

    Code clean-up

--- Python/edu/PP4E/I The Begining/make_db_pickle.py
+++ Python/edu/PP4E/I The Begining/make_db_pickle.py
@@ -1,6 +1,9 @@
-from initdata import db
 import pickle
-# use binary mode files in 3.X
-dbfile = open('people-pickle.edu', 'wb')
-pickle.dump(db, dbfile)                            # data is bytes, not str
-dbfile.close()
+
+
+def pickle_db(object):
+    # use binary mode files in 3.X
+    dbfile = open('people-pickle.edu', 'wb')
+    pickle.dump(object, dbfile)  # data is bytes, not str
+    dbfile.close()
+    return True
