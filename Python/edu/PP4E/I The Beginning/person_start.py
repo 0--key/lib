8207bd4ab34fa9f70d67639653d0e6388b1e6134
a.s.kosinov@gmail.com <a.s.kosinov@gmail.com>
2016-01-23 05:44:15 +0200

    Adding behavior

--- Python/edu/PP4E/I The Beginning/person_start.py
+++ Python/edu/PP4E/I The Beginning/person_start.py
@@ -6,6 +6,12 @@ class Person:
         self.pay = pay
         self.job = job
 
+    def lastName(self):
+        return self.name.split()[-1]
+
+    def giveRaise(self, percent):
+        self.pay *= (1.0 + percent)
+
 if __name__ == '__main__':
     bob = Person('Bob Smith', 42, 30000, 'software')
     sue = Person('Sue Jones', 45, 40000, 'hardware')
