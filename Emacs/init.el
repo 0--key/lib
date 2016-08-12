1b5e2d680d8150ccf82583bef9b2630f6633d470
Anton S. Kosinov <a.s.kosinov@gmail.com>
2016-08-12 18:13:10 +0300

    Fresh color palette

--- Emacs/init.el
+++ Emacs/init.el
@@ -82,9 +82,9 @@
 (global-set-key (kbd "C-c 1") 'color-theme-charcoal-black)
 (global-set-key (kbd "C-c 2") 'color-theme-ld-dark)
 (global-set-key (kbd "C-c 3") 'color-theme-hober)
-(global-set-key (kbd "C-c 4") 'color-theme-calm-forest)
-(global-set-key (kbd "C-c 5") 'color-theme-dark-gnus)
-(global-set-key (kbd "C-c 6") 'color-theme-late-night)
+(global-set-key (kbd "C-c 4") 'color-theme-oswald)
+(global-set-key (kbd "C-c 5") 'color-theme-tty-dark)
+(global-set-key (kbd "C-c 6") 'color-theme-taming-mr-arneson)
 (global-set-key (kbd "C-c 7") 'color-theme-euphoria)
 (global-set-key (kbd "C-c 8") 'color-theme-retro-orange)
 ;;
