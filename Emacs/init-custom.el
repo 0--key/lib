f82146c758a2f7691f9ac32f7d85476186141ac5
Anton S. Kosinov <a.s.kosinov@gmail.com>
2016-11-09 15:39:47 +0200

    Tabs/space indentation preventing trick

--- Emacs/init-custom.el
+++ Emacs/init-custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(indent-tabs-mode nil)
  '(magit-diff-section-arguments (quote ("--no-ext-diff")))
  '(org-confirm-babel-evaluate nil)
  '(org-src-window-setup (quote other-frame))
