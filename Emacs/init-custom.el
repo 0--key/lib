a6b8fccd550c8b90da01fd57665fa240c611bcb6
Anton S. Kosinov <a.s.kosinov@gmail.com>
2017-02-09 21:23:28 +0200

    Emacs25.1 documentation path definition

--- Emacs/init-custom.el
+++ Emacs/init-custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(Info-default-directory-list (quote ("/usr/share/info/" "/usr/share/info/emacs-25/")))
  '(magit-diff-section-arguments (quote ("--no-ext-diff")))
  '(org-confirm-babel-evaluate nil)
  '(org-src-window-setup (quote other-frame))
