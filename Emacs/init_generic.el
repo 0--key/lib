bdff7f850ca81067e3ebfcff652a5710fda0849a
Anton S. Kosinov <a.s.kosinov@gmail.com>
2018-03-16 08:17:10 +0200

    Drill files separation by month

--- Emacs/init_generic.el
+++ Emacs/init_generic.el
@@ -109,7 +109,7 @@
   ))
 ;;
 (require 'voca-builder)
-  (setq voca-builder/voca-file "/usr/local/git/0--key/lib/org/vocabulary_s_2.org")
+  (setq voca-builder/voca-file "/usr/local/git/0--key/lib/org/vocabulary/201803.org")
   (setq voca-builder/export-file "~/.voca-builder-temp.org")
   (setq voca-builder/current-tag "satellite")
 
