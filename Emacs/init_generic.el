bcb8c10e8dca1e87f3b31bb3c17fa105cd4f36db
Anton S. Kosinov <a.s.kosinov@gmail.com>
2018-07-29 17:50:59 +0300

    The superfluous 'custom' section removal

--- Emacs/init_generic.el
+++ Emacs/init_generic.el
@@ -128,27 +128,3 @@
 (setq voca-builder/current-tag "Durrell")
 (setq voca-builder/current-tag "Coelho")
 (setq voca-builder/current-tag "satellite")
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(Info-additional-directory-list
-   (quote
-    ("/usr/share/info/scrapy/" "/usr/share/info/python3.4/" "/usr/share/info/postgres/")))
- '(bmkp-last-as-first-bookmark-file "/home/alioth/.emacs.d/bookmarks")
- '(org-confirm-babel-evaluate nil)
- '(org-modules
-   (quote
-    (org-bbdb org-bibtex org-docview org-gnus org-info org-irc org-mhe org-rmail org-w3m org-drill)))
- '(org-src-window-setup (quote other-frame))
- '(package-selected-packages
-   (quote
-    (htmlize langtool org bookmark+ color-theme elfeed elpy google-translate howdoi key-chord magit marshal org-bullets org-pomodoro pcache travis voca-builder ereader docker))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
