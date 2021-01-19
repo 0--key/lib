a381296fb7ee80f4ed7a34954a27cd60d4887834
Anton S. Kosinov <a.s.kosinov@gmail.com>
2021-01-19 16:47:03 +0200

    Custom configs for Gnus

--- Emacs/custom.el
+++ Emacs/custom.el
@@ -9,7 +9,11 @@
  '(font-use-system-font t)
  '(indent-tabs-mode nil)
  '(package-selected-packages
-   '(python-info org-drill engine-mode ereader google-translate howdoi htmlize key-chord langtool magit marshal org-pomodoro pcache simpleclip travis voca-builder color-theme-modern elpy org-bullets pyvenv)))
+   '(python-info org-drill engine-mode ereader google-translate howdoi htmlize key-chord langtool magit marshal org-pomodoro pcache simpleclip travis voca-builder color-theme-modern elpy org-bullets pyvenv))
+ '(python-shell-interpreter "python3")
+ '(send-mail-function 'smtpmail-send-it)
+ '(smtpmail-smtp-server "smtp.gmail.com")
+ '(smtpmail-smtp-service 587))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
