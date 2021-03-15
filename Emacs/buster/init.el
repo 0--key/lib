e26448223fb554f021f8418757e553f220eafb76
Anton S. Kosinov <a.s.kosinov@gmail.com>
2021-03-15 10:08:41 +0200

    Emacs' satellite config file for Buster

---
+++ Emacs/buster/init.el
@@ -0,0 +1,28 @@
+(require 'package)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+;; and MELPA
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+
+(package-initialize)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(bmkp-last-as-first-bookmark-file "/home/alioth/.emacs.d/bookmarks")
+ '(custom-safe-themes
+   (quote
+    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(key-chord-mode t)
+ '(package-selected-packages
+   (quote
+    (speed-type elpy dash py-autopep8 ereader howdoi org-drill popup voca-builder org-bullets google-translate magit key-chord))))
+
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
