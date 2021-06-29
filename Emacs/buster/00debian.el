043a56103086e987cafe0f0d5836ae7ce7c75175
Anton S. Kosinov <a.s.kosinov@gmail.com>
2021-06-29 13:39:07 +0300

    Configuring for June's vocabulary

--- Emacs/buster/00debian.el
+++ Emacs/buster/00debian.el
@@ -61,7 +61,7 @@
 (defun google-translate--search-tkk () "Search TKK." (list 430675 2721866130))
 
 (require 'voca-builder)
-(setq voca-builder/voca-file "~/git/lib/org/vocabulary/202105.org")
+(setq voca-builder/voca-file "~/git/lib/org/vocabulary/202106.org")
 (setq voca-builder/export-file "~/.voca-builder-temp.org")
 (setq voca-builder/current-tag "misc")
 (setq voca-builder/current-tag "Elephant")
