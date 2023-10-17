a7975e078c195fecd869df4c296a158c503ed0da
Anton S. Kosinov <a.s.kosinov@gmail.com>
2023-10-17 10:43:53 +0100

    Now all org-capture-templates are in a separate file

--- Emacs/buster/00debian.el
+++ Emacs/buster/00debian.el
@@ -56,51 +56,10 @@
 (setq langtool-default-language "en-US")
 
 ;; Additional config section:
-(load "~/emacs.d/key-chords.el")
+(load "~/.emacs.d/key-chords.el")
 
-(setq org-capture-templates
-'(("t" "Todo" entry (file+headline "~/git/lib/org/agenda/might-do.list" "Tasks")
-   "* RAW %?%i\n \n %a")
-  ("i" "Idiom" entry (file+headline "~/git/lib/org/drills.org" "Idioms")
-   "* COMMENT >->->:drill:\n%i")
-  ;;
-  ("p" "Proverb" entry (file+headline "~/git/lib/org/drills.org" "Proverbs")
-   "** COMMENT ==> => >> :drill:
-   :PROPERTIES:
-   :DRILL_CARD_TYPE: hide1cloze
-   :END:\n%?%i\n\n/%c/" :empty-lines 1)
-  ;;
-  ("o" "Obvious proverb" entry (file+headline "~/git/lib/org/drills.org" "Proverbs")
-   "** COMMENT ==> => >> :drill:
-   :PROPERTIES:
-   :DRILL_CARD_TYPE: hide1cloze
-   :END:\n%?%i\n\n" :empty-lines 1)
-  ;;
-  ("w" "Quote" entry (file+headline "~/git/lib/org/drills.org" "Quotes")
-   "* COMMENT >->-> %c :drill:
-   :PROPERTIES:
-   :DRILL_CARD_TYPE: hide1cloze
-   :END:\n%?%i\n")
-  ;;
-  ("e" "AnonQuote" entry (file+headline "~/git/lib/org/drills.org" "Quotes")
-   "** COMMENT ==> => >> :drill:
-   :PROPERTIES:
-   :DRILL_CARD_TYPE: hide1cloze
-   :END:\n%?%i\n\n" :empty-lines 1)
-  ;;
-  ("n" "Notes");; <-- prefix key for notes
-("nf" "Fleeting" entry (file+headline
-			"~/git/lib/org/agenda/might-do.list" "Notes")
- "* RAW %?%i")
-("ni" "Initial" entry (file+headline
-		       "~/git/lib/org/agenda/might-do.list" "Notes")
- "* INIT %?%i")
-("np" "Pythonic" entry (file+headline
-		       "~/git/lib/org/agenda/py-genda.list" "Notes")
- "* INIT %?%i")
-   ;; ‘%k’ Title of the currently clocked task.
-))
-
+;; Org-capture templates in a separate file:
+(load "~/.emacs.d/org-capture-templates.el")
 
 ;; Jekyll settings there -->
 ;;
---
+++ Emacs/buster/org-capture-templates.el
@@ -0,0 +1,45 @@
+;; No comments
+(setq org-capture-templates
+'(("t" "Todo" entry (file+headline "~/git/lib/org/agenda/might-do.list" "Tasks")
+   "* RAW %?%i\n \n %a")
+  ("i" "Idiom" entry (file+headline "~/git/lib/org/drills.org" "Idioms")
+   "* COMMENT >->->:drill:\n%i")
+  ;;
+  ("p" "Proverb" entry (file+headline "~/git/lib/org/drills.org" "Proverbs")
+   "** COMMENT ==> => >> :drill:
+   :PROPERTIES:
+   :DRILL_CARD_TYPE: hide1cloze
+   :END:\n%?%i\n\n/%c/" :empty-lines 1)
+  ;;
+  ("o" "Obvious proverb" entry (file+headline "~/git/lib/org/drills.org" "Proverbs")
+   "** COMMENT ==> => >> :drill:
+   :PROPERTIES:
+   :DRILL_CARD_TYPE: hide1cloze
+   :END:\n%?%i\n\n" :empty-lines 1)
+  ;;
+  ("w" "Quote" entry (file+headline "~/git/lib/org/drills.org" "Quotes")
+   "* COMMENT >->-> %c :drill:
+   :PROPERTIES:
+   :DRILL_CARD_TYPE: hide1cloze
+   :END:\n%?%i\n")
+  ;;
+  ("e" "AnonQuote" entry (file+headline "~/git/lib/org/drills.org" "Quotes")
+   "** COMMENT ==> => >> :drill:
+   :PROPERTIES:
+   :DRILL_CARD_TYPE: hide1cloze
+   :END:\n%?%i\n\n" :empty-lines 1)
+  ;;
+  ("n" "Notes");; <-- prefix key for notes
+("nf" "Fleeting" entry (file+headline
+			"~/git/lib/org/agenda/might-do.list" "Notes")
+ "* RAW %?%i")
+("ni" "Initial" entry (file+headline
+		       "~/git/lib/org/agenda/might-do.list" "Notes")
+ "* INIT %?%i")
+("np" "Pythonic" entry (file+headline
+		       "~/git/lib/org/agenda/py-genda.list" "Notes")
+ "* INIT %?%i")
+   ;; ‘%k’ Title of the currently clocked task.
+))
+
+ 
