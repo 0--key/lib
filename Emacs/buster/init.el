060ced456d893b4816aa5fc02c6af7b6e2cddc16
Anton S. Kosinov <a.s.kosinov@gmail.com>
2023-11-10 09:26:02 +0000

    Friday workspace cleansing

--- Emacs/buster/init.el
+++ Emacs/buster/init.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(bmkp-last-as-first-bookmark-file "~/.emacs.d/bookmarks")
+ '(bmkp-last-as-first-bookmark-file "/home/alioth/.emacs.d/bookmarks")
  '(browse-url-browser-function 'eww-browse-url)
  '(custom-safe-themes
    '("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default))
