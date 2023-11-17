1268cd1b2a1ee17ea795dba8def8e2ad4f74626b
Anton S. Kosinov <a.s.kosinov@gmail.com>
2023-11-17 17:59:52 +0000

    A pile of key-chords extension

--- Emacs/buster/key-chords.el
+++ Emacs/buster/key-chords.el
@@ -8,7 +8,10 @@
 
 ;; Scrolling
 (key-chord-define-global "ij"     'scroll-down-command)
-(key-chord-define-global ",j"     'scroll-up-command)
+(key-chord-define-global "km"     'scroll-up-command)
+(key-chord-define-global "IJ"     'scroll-other-window-down)
+(key-chord-define-global "KM"     'scroll-other-window)
+
 
 ;; Frames and Windows management:
 (key-chord-define-global "'\\"     'other-frame)
@@ -25,15 +28,18 @@
 (key-chord-define-global "}|"
 			 (lambda ()
 			   (interactive) (other-frame -1)))
+
+;; Registers
 (key-chord-define-global "rw"     'window-configuration-to-register)
 (key-chord-define-global "rj"     'jump-to-register)
-(key-chord-define-global "fm"     'follow-mode)
+
 ;; Buffers
 (key-chord-define-global "bn"     'list-buffers)
 (key-chord-define-global "bv"     'switch-to-buffer)
 (key-chord-define-global "fq"     'previous-buffer)
 (key-chord-define-global "jp"     'next-buffer)
 (key-chord-define-global "bk"     'kill-buffer)
+(key-chord-define-global "b4"     'switch-to-buffer-other-window)
 (key-chord-define-global "sq"
 			 (lambda ()
 			   (interactive) (save-some-buffers 1)))
@@ -55,6 +61,7 @@
 (key-chord-define-global "dc"     'with-editor-finish)
 (key-chord-define-global "gs"     'magit-status)
 (key-chord-define-global "vl"     'vc-print-log)
+(key-chord-define-global "fm"     'follow-mode)
 ;;
 ;; (key-chord-define-global "ri"     'insert-register) ;; too common
 
