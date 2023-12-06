110b4a28164025b2d9c8190bb6ebd8780bf2331c
Anton S. Kosinov <a.s.kosinov@gmail.com>
2023-12-06 14:17:38 +0000

    Common for all local users settings

--- Emacs/bookworm/00debian.el
+++ Emacs/bookworm/00debian.el
@@ -1,3 +1,10 @@
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+
+(setq calendar-latitude 55.0408104)
+   (setq calendar-longitude -7.65058411)
+   (setq calendar-location-name "Baile na Bó, Dún na nGall")
 ;; Set the default mail server and news server as specified by Debian
 ;; policy.
 
