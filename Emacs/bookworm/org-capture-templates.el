1b016b685b1d5d5f8779ac9f36d05113fe09fc13
Anton S. Kosinov <a.s.kosinov@gmail.com>
2023-12-11 19:00:15 +0000

    The ~PATH~ for org-capturing is set and tested

--- Emacs/bookworm/org-capture-templates.el
+++ Emacs/bookworm/org-capture-templates.el
@@ -2,36 +2,36 @@
 (setq org-capture-templates
       '(;; A new and raw task into initial agenda file
 	("t" "Todo" entry (file+headline
-			   "~/git/lib/org/agenda/might-do.list" "Tasks")
+			   "~/Git/0--key/lib/org/agenda/might-do.list" "Tasks")
 	 "* RAW %?%i\n \n %a")
 	;;
 	("i" "Idiom" entry (file+headline
-			    "~/git/lib/org/drills.org" "Idioms")
+			    "~/Git/0--key/lib/org/drills.org" "Idioms")
 	 "* COMMENT >->->:drill:\n%i")
 	;;
 	("p" "Proverb" entry (file+headline
-			      "~/git/lib/org/drills.org" "Proverbs")
+			      "~/Git/0--key/lib/org/drills.org" "Proverbs")
 	 "** COMMENT ==> => >> :drill:
    :PROPERTIES:
    :DRILL_CARD_TYPE: hide1cloze
    :END:\n%?%i\n\n/%c/" :empty-lines 1)
 	;;
 	("o" "Obvious proverb" entry (file+headline
-				      "~/git/lib/org/drills.org" "Proverbs")
+				      "~/Git/0--key/lib/org/drills.org" "Proverbs")
 	 "** COMMENT ==> => >> :drill:
    :PROPERTIES:
    :DRILL_CARD_TYPE: hide1cloze
    :END:\n%?%i\n\n" :empty-lines 1)
 	;;
 	("w" "Quote" entry (file+headline
-			    "~/git/lib/org/drills.org" "Quotes")
+			    "~/Git/0--key/lib/org/drills.org" "Quotes")
 	 "* COMMENT >->-> %c :drill:
    :PROPERTIES:
    :DRILL_CARD_TYPE: hide1cloze
    :END:\n%?%i\n")
 	;;
 	("e" "AnonQuote" entry (file+headline
-				"~/git/lib/org/drills.org" "Quotes")
+				"~/Git/0--key/lib/org/drills.org" "Quotes")
 	 "** COMMENT ==> => >> :drill:
    :PROPERTIES:
    :DRILL_CARD_TYPE: hide1cloze
@@ -39,7 +39,7 @@
 	;;
 	;;
 	("h" "Thank You" entry (file+headline
-				"~/git/lib/org/drills.org" "How to say 'Thank You!'")
+				"~/Git/0--key/lib/org/drills.org" "How to say 'Thank You!'")
 	 "** COMMENT ==> => >> :drill:
    :PROPERTIES:
    :DRILL_CARD_TYPE: hide1cloze
@@ -49,18 +49,18 @@
 	("n" "Notes");; <-- prefix key for notes
 	;; For the crude notes only:
 	("nf" "Fleeting" entry (file+headline
-			"~/git/lib/org/agenda/might-do.list" "Notes")
+			"~/Git/0--key/lib/org/agenda/might-do.list" "Notes")
 	 "* RAW %?%i"
 	 :immediate-finish 1
 	 :empty-lines-after 1)
 	;;
 	("ni" "Initial" entry (file+headline
-		       "~/git/lib/org/agenda/might-do.list" "Notes")
+		       "~/Git/0--key/lib/org/agenda/might-do.list" "Notes")
 	 "* INIT %i%?\nSCHEDULED: %t\n"
 	 :immediate-finish 1
 	 :empty-lines-after 1)
 	("np" "Pythonic" entry (file+headline
-			"~/git/lib/org/agenda/py-genda.list" "Notes")
+			"~/Git/0--key/lib/org/agenda/py-genda.list" "Notes")
 	 "* INIT %i%?\nSCHEDULED: %t\n"
 	 :immediate-finish 1
 	 :empty-lines-after 1)
