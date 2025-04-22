00607d1fb9c018aae62d8774b7aec65118accd48
Anton S. Kosinov <a.s.kosinov@gmail.com>
2025-04-22 15:55:48 +0100

    Minor corrections

--- Emacs/bookworm/emacsHP.el
+++ Emacs/bookworm/emacsHP.el
@@ -114,7 +114,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(bmkp-last-as-first-bookmark-file "~/.emacs.d/bookmarks")
+ '(bmkp-last-as-first-bookmark-file "/home/alioth/.emacs.d/bookmarks")
  '(browse-url-browser-function 'eww-browse-url)
  '(desktop-save-mode t)
  '(org-agenda-files
